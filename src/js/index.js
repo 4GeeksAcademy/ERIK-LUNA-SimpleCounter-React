5f8b418adfc56ea8ff25fafb5d9a2a5496eaeaf0
Runneker74 <122167596+Runneker74@users.noreply.github.com>
2023-09-06 21:48:15 +0000

    codigo SetInterval

--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,34 @@
 //import react into the bundle
-import React from "react";
 import ReactDOM from "react-dom";
+import React, { useState, useEffect } from 'react';
 
 // include your styles into the webpack bundle
 import "../styles/index.css";
 
 //import your own components
-import Home from "./component/home.jsx";
 
-//render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+function SecondsCounter() {
+    
+  }
+
+  let seconds = 0;
+
+  const Contador = setInterval(() => {
+  seconds++;
+
+  if (seconds === 20) {
+    clearInterval(Contador); 
+  }
+
+
+  console.log(`Segundos transcurridos: ${seconds}`);
+
+  }, 1000);
+    
+
+
+//render your react applicatio
+ReactDOM.render(<SecondsCounter 
+    seconds="Contador"
+    
+    />, document.querySelector("#app"));
